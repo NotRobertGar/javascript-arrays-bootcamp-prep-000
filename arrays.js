34b35baa495894afa1015538933f9e82dbd46ec8
cab driver <cocktailsthecomic@gmail.com>
2017-09-04 14:29:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, number){
   return [number, ...array];
 }
+
+function destructivelyAddElementToBeginningOfArray(array, number){
+  return array.unshift(number);
+}
